8df393677cf38d03debac2e7463cd7d4b1f1af3f
dancroker <160754622+dancroker@users.noreply.github.com>
2025-06-20 21:16:22 +0100

    Update C++ Basic Operations.cpp
    Added: Function Return Types
    Added: Preprocessor Directive
    Added: Scope Resolution Operator
    Changed: Moved main() to the bottom
    Changed: PrintOnTerminal and main to be void

--- C++ Basic Operations.cpp
+++ C++ Basic Operations.cpp
@@ -1,10 +1,60 @@
-#include <iostream>
-
 //To Start Coding:
 //1. Open Developer Command Prompt
 //2. Type "code"
 
-int main() {
+//-------------------------------------------------
+//#include Is A Preprocessor Directive
+//It Tells Compiler To Get Contents Of Another File Before Compiling
+//Like A Python import Operation
+#include <iostream>
+
+//-------------------------------------------------
+// Scope Resolution Operator
+//(__ :: __)
+//Tells Compiler To Look Inside This Namespace/Class/Struct For Specified Something
+//std::string (Look In Standard Libary For "string")
+//To Assume Everything Is std:: (Or Any __:: Unless Specified), Use The Following...
+//using namespace ___; std In sid:: Case
+
+//-------------------------------------------------
+//Function Return Types
+//(What The Function Is Expected / Needs To Return)
+//Every Function Needs To Have This!
+
+//1.Returns Nothing
+void VoidReturningFunction();
+
+//2.Returns Intetger 
+int IntReturningFunction();
+
+//3.Returns Float
+float FloatReturningFunction();
+
+//4.Returns Double
+double DoubleReturningFunction();
+
+//5.Returns Character (Single!)
+char CharReturningFunction();
+
+//6.Returns Boolean (true/false)
+bool BooleanReturningFunction();
+
+//7.Returns String
+#include <string>
+std::string StringReturningFunction();
+//#include Is Required For A Function Returning A String
+//Best To Always #include <string>, 98% Of String Operations Require It
+
+
+//-------------------------------------------------
+//Function To Print
+void PrintOnTerminal() {
     std::cout << "Hello, World!" << std::endl;
-    return 0;
+}
+
+//-------------------------------------------------
+//Main Function Is Whats Run When Program Is Ran
+//(All Functions Need To Be Here!)
+void main() {
+    PrintOnTerminal();
 }
